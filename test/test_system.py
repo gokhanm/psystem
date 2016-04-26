530f1c3bc972066b558bfe7a80d0a3e789f01d8f
Gokhan MANKARA <gokhan@mankara.org>
2016-04-26 16:44:14 +0300

    remove test stats in system module

--- test/test_system.py
+++ test/test_system.py
@@ -29,51 +29,6 @@ class SystemTestCase(unittest.TestCase):
 
         self.assertEqual(new_hostname, current_hostname)
 
-    def test_memory_usage(self):
-
-        gt = system.Get()
-        memory_info = gt.memory_usage
-
-        self.assertIsInstance(memory_info['virtual'], dict)
-        self.assertIsInstance(memory_info['swap'], dict)
-
-        self.assertIsInstance(memory_info['virtual']['percent'], float)
-        self.assertIsInstance(memory_info['virtual']['total'], str)
-        self.assertIsInstance(memory_info['virtual']['free'], str)
-        self.assertIsInstance(memory_info['virtual']['used'], str)
-
-        self.assertIsInstance(memory_info['swap']['percent'], float)
-        self.assertIsInstance(memory_info['swap']['total'], str)
-        self.assertIsInstance(memory_info['swap']['free'], str)
-        self.assertIsInstance(memory_info['swap']['used'], str)
-
-    def test_disk_usage(self):
-
-        gt = system.Get()
-
-        disk_info = gt.disk_usage
-
-        for k, v in disk_info.items():
-
-            self.assertIsInstance(v, dict)
-        
-            self.assertIsInstance(v['total'], str)
-            self.assertIsInstance(v['used'], str)
-            self.assertIsInstance(v['free'], str)
-            self.assertIsInstance(v['percent'], float)
-
-    def test_cpu_percent_usage(self):
-
-        gt = system.Get()
-
-        cpu_usage = gt.cpu_percent_usage()
-
-        self.assertIsInstance(cpu_usage, float)
-
-        cpu_per_usage = gt.cpu_percent_usage(percpu=True)
-
-        self.assertIsInstance(cpu_per_usage, list)
-
     def test_ntp_current_time(self):
 
         gt = system.Get()
