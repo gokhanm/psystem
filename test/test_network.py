06f994b8720e75e135ec59de9b499fb98b40a6f6
Gokhan MANKARA <gokhan@mankara.org>
2016-08-05 19:40:19 +0300

    removed blank lines

--- test/test_network.py
+++ test/test_network.py
@@ -11,16 +11,15 @@ warnings.simplefilter("ignore", ResourceWarning)
 
 
 class NetworkTestCase(unittest.TestCase):
-
     def test_get_interfaces(self):
         get = network.Get()
         interfaces = get.interfaces
         self.assertIsInstance(interfaces, list)
+
         return interfaces
 
     def test_get_ip(self):
         get = network.Get()
-
         interface_list = self.test_get_interfaces()
         
         for i in interface_list:
@@ -42,73 +41,55 @@ class NetworkTestCase(unittest.TestCase):
 
     def test_all_getaways(self):
         get = network.Get()
-
         all_gw = get.all_gateways
         self.assertIsInstance(all_gw, dict)
 
     def test_is_up(self):
-
         get = network.Get()
         result = get.is_up('eth0')
-
         self.assertIsInstance(result, bool)
 
     def test_wrong_typo_interface_name_exception(self):
         get = network.Get()
-
         self.assertRaises(WrongInterfaceName, get.ip, 'ethh1')
 
     def test_set_ip(self):
         st = network.Set()
         get = network.Get()
-
         set_ip = st.ip('eth0', '10.41.0.164')
         current_ip = get.ip('eth0')[0]['addr']
-
         self.assertEqual(current_ip, '10.41.0.164')
 
     def test_false_interface_name_set_ip(self):
         st = network.Set()
-
         self.assertRaises(WrongInterfaceName, st.ip, 'eth', '10.41.0.164')
 
     def test_not_valid_ipv4_address_set(self):
         st = network.Set()
-
         self.assertRaises(NotValidIPv4Address, st.ip, 'eth0', '10.41.0')
 
     def test_get_netmask(self):
-
         gt = network.Get()
-
         ip = gt.netmask('eth0')
         self.assertIsInstance(ip, str)
 
     def test_get_no_valid_netmask(self):
-
         gt = network.Get()
         self.assertRaises(WrongInterfaceName, gt.netmask, 'eth')
 
     def test_set_netmask(self):
-
         st = network.Set()
         gt = network.Get()
-
         set_netmask = st.netmask('eth0', '255.255.255.0')
-
         current_netmask = gt.netmask('eth0')
-
         self.assertEqual(current_netmask, '255.255.255.0')
 
     def test_set_netmask_no_valid_interface_name(self):
         st = network.Set()
-
         self.assertRaises(WrongInterfaceName, st.netmask, 'ethh', '255.255.255.0')
 
     def test_set_netmask_no_valid_ip_address(self):
-
         st = network.Set()
-
         self.assertRaises(NotValidIPv4Address, st.netmask, 'eth0', '255.255.0')
 
 
--- test/test_system.py
+++ test/test_system.py
@@ -7,55 +7,37 @@ __email__ = "gokhan@mankara.org"
 
 
 class SystemTestCase(unittest.TestCase):
-
     def test_get_hostname(self):
-
         gt = system.Get()
-
         hostname = gt.hostname
-
         self.assertIsInstance(hostname, str)
 
     def test_set_hostname(self):
-
         st = system.Set()
         gt = system.Get()
-
         new_hostname = "local.local"
-
         st.hostname(new_hostname)
-
         current_hostname = gt.hostname
-
         self.assertEqual(new_hostname, current_hostname)
 
     def test_ntp_current_time(self):
-
         gt = system.Get()
-
         current_time = gt.ntp_current_time
-
         self.assertIsInstance(current_time, str)
 
     def test_current_time(self):
-
         gt = system.Get()
         t = gt.current_time
-
         self.assertIsInstance(t, str)
 
     def test_uptime(self):
-
         gt = system.Get()
         uptime = gt.uptime
-
         self.assertIsInstance(uptime, str)
 
     def test_pid(self):
         gt = system.Get()
-
         pid = gt.pid('crypto')
-
         self.assertIsInstance(pid, int)
 
 
