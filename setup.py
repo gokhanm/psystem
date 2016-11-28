118940c843108237faa3156f7dc9c2e8149bbe68
Gokhan MANKARA <gokhan@mankara.org>
2016-11-28 20:39:19 +0300

    fix License Approved

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'License :: OSI Approved :: GNU License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: System :: Systems Administration',
-
     ]
 )
