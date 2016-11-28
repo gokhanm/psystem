715e1ec6ca94ea097ec16805a3153da88bb0eeb8
Gokhan MANKARA <gokhan@mankara.org>
2016-11-28 20:28:46 +0300

    setup.cfg package

--- setup.py
+++ setup.py
@@ -6,13 +6,31 @@ def read(fname):
 
 setup(
     name = "psystem",
-    version = "0.1.1",
+    version = "0.2.0",
     author = "Gokhan Mankara",
     author_email = "gokhan@mankara.org",
     description = ("Linux System Python Module"),
     license = "GNU GENERAL PUBLIC LICENSE",
-    keywords = "linux system",
-    url = "",
+    keywords = ["linux system", "network", "mail", "shell", "log"],
+    url = "https://github.com/gokhanm/psystem",
     packages=['psystem', 'test'],
     long_description=read('README.md'),
+    install_requires=list(filter(None, [
+        'netifaces',
+        'pyroute2',
+        'validators',
+        'psutil',
+        'spur',
+    ])),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: GNU License',
+        'Topic :: System :: Systems Administration',
+
+    ]
 )
